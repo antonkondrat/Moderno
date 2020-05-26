b08c2888390d96fa6ea1efabeb74a6c73d75b49f
antonkondrat <antonboec@mail.ru>
2020-05-26 22:58:23 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,46 @@
-$(function(){
-
-   var mixer = mixitup('.products__inner-box');
-
-   $('.rate-star').rateYo({
+$(function () {
+   $(".rate-star").rateYo({
       rating: 5,
-      starWidth: '12px',
-      readOnly: true
+      starWidth: "12px",
+      readOnly: true,
    });
 
-   $('.product-slider__inner').slick({
+   $(".product-slider__inner").slick({
       dots: true,
       arrows: false,
       slidesToShow: 4,
-      slidesToScroll: 2
+      slidesToScroll: 2,
    });
 
+   $("#range").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 200,
+      to: 600,
+      prefix: "$",
+   });
+
+   $(".icon-th-list").on("click", function () {
+      // $('.product__item').addClass('list');
+      $(".product-page__content .product__item").addClass("list");
+      $(".icon-th-list").addClass("active");
+      $(".icon-th-large").removeClass("active");
+   });
+   $(".icon-th-large").on("click", function () {
+      // $('.product__item').removeClass('list');
+      $(".product-page__content .product__item").removeClass("list");
+      $(".icon-th-large").addClass("active");
+      $(".icon-th-list").removeClass("active");
+   });
+
+   $(".menu__btn").on("click", function () {
+      $(".menu__list").slideToggle();
+   });
+
+   $(".header__btn-menu").on("click", function () {
+      $(".header__box").toggleClass('active');
+   });
+
+   var mixer = mixitup(".products__inner-box");
 });
