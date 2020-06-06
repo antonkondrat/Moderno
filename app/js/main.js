d4b966560025a5e86d4b1f6b057a8152865c1717
antonkondrat <antonboec@mail.ru>
2020-06-06 16:08:56 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,23 @@
 $(function () {
-   $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
-      var id = $(this).attr('data-id');
-         $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-         $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
-         $(this).addClass('active');
-         $('#'+id).addClass('active-tab').fadeIn();
-         return false;
-      });
+   $("input[type='file'], select").styler();
+
+   $(".product-one__tabs .tab, .settings__tabs .tab").on("click", function (
+      event
+   ) {
+      var id = $(this).attr("data-id");
+      $(".product-one__tabs, .settings__tabs")
+         .find(".tab-item")
+         .removeClass("active-tab")
+         .hide();
+      $(".product-one__tabs .tabs, .settings__tabs .tabs")
+         .find(".tab")
+         .removeClass("active");
+      $(this).addClass("active");
+      $("#" + id)
+         .addClass("active-tab")
+         .fadeIn();
+      return false;
+   });
 
    $(".rate-star").rateYo({
       rating: 5,
@@ -19,6 +30,26 @@ $(function () {
       arrows: false,
       slidesToShow: 4,
       slidesToScroll: 2,
+      responsive: [
+         {
+            breakpoint: 1900,
+            settings: {
+               slidesToShow: 3
+            }
+         },
+         {
+            breakpoint: 1441,
+            settings: {
+               slidesToShow: 2
+            }
+         },
+         {
+            breakpoint: 801,
+            settings: {
+               slidesToShow: 1
+            }
+         },
+      ]
    });
 
    $("#range").ionRangeSlider({
@@ -48,8 +79,9 @@ $(function () {
    });
 
    $(".header__btn-menu").on("click", function () {
-      $(".header__box").toggleClass('active');
+      $(".header__box").toggleClass("active");
    });
 
    var mixer = mixitup(".products__inner-box");
+
 });
